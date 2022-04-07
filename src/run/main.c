8338b0fa3a2f2991bdf9ef809c6a0854d2f0fff9
JeppeSS <jsskov92@gmail.com>
2022-04-07 20:49:32 +0200

    Added user input

--- src/run/main.c
+++ src/run/main.c
@@ -10,6 +10,15 @@ main( void ){
     const int num_to_guess = hgl_rand_int( -5, 5 );
 
     fprintf( stdout, "Secret number to guess %d\n", num_to_guess );
-    
+
+
+    char input[ 256 ] = { 0 };
+    if( !fgets( input, sizeof( input ), stdin) ) {
+        fprintf( stderr, "Something went wrong. Could not read user input." );
+        return EXIT_FAILURE;
+    }
+
+    fprintf( stdout, "User input: %s\n", input );
+
     return EXIT_SUCCESS;
 }
