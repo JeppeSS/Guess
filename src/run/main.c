21544ceaa0f315426cfa1fb6e88937d46be3d1cf
JeppeSS <jsskov92@gmail.com>
2022-04-12 10:55:41 +0200

    Game logic progress

--- src/run/main.c
+++ src/run/main.c
@@ -11,16 +11,27 @@ typedef struct {
     unsigned int allowed_attempts;
     unsigned int attempts_used;
 
-} guess_state;
+} game_state;
 
-guess_state
-guess_state_new( unsigned int allowed_attempts );
+typedef enum {
+    HIGH,
+    LOW,
+    EQUAL,
+
+} guess_status;
+
+game_state
+game_state_new( unsigned int allowed_attempts );
+
+guess_status
+process_guess( int number_to_guess, int guess );
+
 
 int
 main( void ){
     // Initialize
     hgl_rand_seed( time( NULL ) );
-    guess_state game = guess_state_new( 3 );
+    game_state game = game_state_new( 3 );
 
     const size_t MAX_INPUT_SIZE = 128;
 
@@ -28,6 +39,8 @@ main( void ){
     const hgl_str enter_num_txt     = hgl_str_new( "Enter a number:" );
     const hgl_str invalid_input_txt = hgl_str_new( "Invalid input, try again." ); 
     const hgl_str win_txt           = hgl_str_new( "You win!");
+    const hgl_str too_high_txt      = hgl_str_new( "Guess is too high!" );
+    const hgl_str too_low_txt       = hgl_str_new( "Guess is too low!" );
 
 
     bool is_running = true;
@@ -38,11 +51,18 @@ main( void ){
         if( user_input.is_valid ) {
             const hgl_str_int guess_input = hgl_str_parse_int( user_input.result );
             if( guess_input.is_valid ) {
-                const int guess = guess_input.result;
-                if( guess == game.number_to_guess ) {
-                    fprintf( stdout, "%s\n", win_txt.p_chars );
+                const guess_status status = process_guess( game.number_to_guess, guess_input.result );
+                if( status == EQUAL ) {
+                    fprintf( stdout, "%s\n\n", win_txt.p_chars );
                     is_running = false;
+                } else if ( status == HIGH ) {
+                    fprintf( stdout, "%s\n\n", too_high_txt.p_chars );
+                    game.attempts_used++;
+                } else {
+                    fprintf( stdout, "%s\n\n", too_low_txt.p_chars );
+                    game.attempts_used++;
                 }
+
             } else {
                 fprintf( stdout, "%s\n\n", invalid_input_txt.p_chars ); 
             }
@@ -58,12 +78,24 @@ main( void ){
     return EXIT_SUCCESS;
 }
 
-guess_state
-guess_state_new( unsigned int allowed_attempts ) {
-    return ( guess_state ) {
+game_state
+game_state_new( unsigned int allowed_attempts ) {
+    return ( game_state ) {
         .allowed_attempts = allowed_attempts,
         .attempts_used    = 0,
         .number_to_guess = hgl_rand_int( 0, 10 )
     };
+}
+
+
+guess_status
+process_guess( int number_to_guess, int guess ) {
+    if( guess > number_to_guess ) {
+        return HIGH;
+    } else if ( guess < number_to_guess ) {
+        return LOW;
+    }
+
+    return EQUAL;
 }
 
