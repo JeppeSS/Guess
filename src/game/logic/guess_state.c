4e18ac6a54c27810cf1f3ca3066b7aa466b4ac3a
JeppeSS <jsskov92@gmail.com>
2022-04-28 20:23:32 +0200

    Fix attempt count!

--- src/game/logic/guess_state.c
+++ src/game/logic/guess_state.c
@@ -15,7 +15,7 @@ guess_state_new( int16_t lower_bound, int16_t upper_bound, uint8_t allowed_attem
 
 guess_status
 guess_state_make_guess( guess_state *p_state, int16_t guess ) {
-    if( p_state->allowed_attempts == p_state->attempts_used ){
+    if( ( p_state->allowed_attempts - 1 ) == p_state->attempts_used ){
         return LOSE_STATUS;
     } else { 
         p_state->attempts_used++;
