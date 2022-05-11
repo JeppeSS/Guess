f1e69aef6bd58038a20f630b31c4b0a75c36a138
JeppeSS <jsskov92@gmail.com>
2022-05-11 22:01:48 +0200

    Create build folder if not present

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
+mkdir -p build
 clang -std=c17 -Wall -Wextra -Isrc/ src/core/random/hgl_rand.c src/core/string/hgl_str.c src/core/console/hgl_input.c src/game/logic/guess_state.c src/game/guessing.c src/run/main.c -o build/Guess
