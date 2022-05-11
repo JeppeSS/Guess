4421c2250722c2a81f5127392e934661461f9367
JeppeSS <jsskov92@gmail.com>
2022-05-11 22:19:41 +0200

    File fetching in build script

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@ cc="clang"
 version="-std=c17"
 warnings="-Wall -Wextra"
 
-root_dir="-Isrc/"
 output_dir="build"
+output_name="Guess"
+
+root_dir="-Isrc/"
+files=$( find src | grep "\.c$")
 
 mkdir -p $output_dir
-$cc $version $warnings $root_dir src/core/random/hgl_rand.c src/core/string/hgl_str.c src/core/console/hgl_input.c src/game/logic/guess_state.c src/game/guessing.c src/run/main.c -o $output_dir/Guess
+$cc $version $warnings $root_dir $files -o $output_dir/$output_name
