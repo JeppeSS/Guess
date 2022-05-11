3539ebe38d8ac4663d8353e3ad677c95e2a123b8
JeppeSS <jsskov92@gmail.com>
2022-05-11 22:13:08 +0200

    Added build variables

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
-mkdir -p build
-clang -std=c17 -Wall -Wextra -Isrc/ src/core/random/hgl_rand.c src/core/string/hgl_str.c src/core/console/hgl_input.c src/game/logic/guess_state.c src/game/guessing.c src/run/main.c -o build/Guess
+cc="clang"
+version="-std=c17"
+warnings="-Wall -Wextra"
+
+root_dir="-Isrc/"
+output_dir="build"
+
+mkdir -p $output_dir
+$cc $version $warnings $root_dir src/core/random/hgl_rand.c src/core/string/hgl_str.c src/core/console/hgl_input.c src/game/logic/guess_state.c src/game/guessing.c src/run/main.c -o $output_dir/Guess
