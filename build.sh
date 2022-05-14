4feb0b4b8a31c7f44a97d8a668b15aa2dba9d72d
JeppeSS <jsskov92@gmail.com>
2022-05-14 22:59:22 +0200

    Added debug and release option to build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 cc="clang"
 version="c17"
 warnings="-Wall -Wextra -Wshadow -Wdouble-promotion -Wformat=2 -Wundef -Wconversion -Werror"
+optimization="-O2"
 
 output_dir="build"
 output_name="Guess"
@@ -8,5 +9,19 @@ output_name="Guess"
 root_dir="-Isrc/"
 files=$( find src | grep "\.c$")
 
-mkdir -p $output_dir
-$cc -std=$version $warnings $root_dir $files -o $output_dir/$output_name
+while getopts ':rd' opt; do
+    case "$opt" in
+        r)
+            mkdir -p $output_dir/release
+            $cc -std=$version $optimization $warnings $root_dir $files -o $output_dir/release/$output_name
+            exit;;
+        d)
+            mkdir -p $output_dir/debug
+            $cc -std=$version -g $warnings $root_dir $files -o $output_dir/debug/$output_name
+            exit;;
+        \?)
+            mkdir -p $output_dir/debug
+            $cc -std=$version -g $warnings $root_dir $files -o $output_dir/debug/$output_name
+            exit;;
+    esac
+done
