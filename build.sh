8fdb06b0a40a56efba38155047a56571feca5cec
JeppeSS <jsskov92@gmail.com>
2022-05-14 23:04:13 +0200

    Adjust build

--- build.sh
+++ build.sh
@@ -9,16 +9,16 @@ output_name="Guess"
 root_dir="-Isrc/"
 files=$( find src | grep "\.c$")
 
-while getopts ':rd' opt; do
+while getopts 'rd' opt; do
     case "$opt" in
         r)
             mkdir -p $output_dir/release
             $cc -std=$version $optimization $warnings $root_dir $files -o $output_dir/release/$output_name
-            exit;;
+            ;;
         d)
             mkdir -p $output_dir/debug
             $cc -std=$version -g $warnings $root_dir $files -o $output_dir/debug/$output_name
-            exit;;
+            ;;
         \?)
             mkdir -p $output_dir/debug
             $cc -std=$version -g $warnings $root_dir $files -o $output_dir/debug/$output_name
