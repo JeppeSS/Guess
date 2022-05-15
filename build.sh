ed49ba8e67f1bd0ddefd5710e2cc724e1940cb58
JeppeSS <jsskov92@gmail.com>
2022-05-15 21:01:13 +0200

    Cleanup build script + added run flag

--- build.sh
+++ build.sh
@@ -9,22 +9,61 @@ output_name="Guess"
 root_dir="-Isrc/"
 files=$( find src | grep "\.c$")
 
-while getopts 'rdc' opt; do
+
+should_build_release=false
+should_build_debug=false
+should_clean=false
+should_execute=false
+while getopts 'rdce' opt; do
     case "$opt" in
         r)
-            mkdir -p $output_dir/release
-            $cc -std=$version $optimization $warnings $root_dir $files -o $output_dir/release/$output_name
+            should_build_release=true;
             ;;
         d)
-            mkdir -p $output_dir/debug
-            $cc -std=$version -g $warnings $root_dir $files -o $output_dir/debug/$output_name
+            should_build_debug=true
             ;;
         c)
-            rm -r $output_dir
+            should_clean=true
+            ;;
+        e)
+            should_execute=true
             ;;
         \?)
-            mkdir -p $output_dir/debug
-            $cc -std=$version -g $warnings $root_dir $files -o $output_dir/debug/$output_name
-            exit;;
+            should_clean=true
+            should_build_debug=true
+            should_build_release=true
+            ;;
     esac
 done
+
+
+build_application () {
+    if [ "$should_clean" = true ] ; then
+        rm -rf $output_dir
+    fi
+
+    if [ "$should_build_debug" = true ] ; then
+        mkdir -p $output_dir/debug
+        $cc -std=$version -g $warnings $root_dir $files -o $output_dir/debug/$output_name
+    fi
+
+    if [ "$should_build_release" = true ] ; then
+        mkdir -p $output_dir/release
+        $cc -std=$version $optimization $warnings $root_dir $files -o $output_dir/release/$output_name
+    fi
+
+    if [ "$should_execute" = true ] ; then
+        if [ "$should_build_debug" = true ] ; then
+            ./$output_dir/debug/$output_name
+            exit
+        fi
+
+        if [ "$should_build_release" = true ] ; then
+            ./$output_dir/release/$output_name
+            exit
+        fi
+    fi
+}
+
+
+build_application
